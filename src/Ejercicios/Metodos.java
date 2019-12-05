ec1181282968f10738bb5af834d7e867dee9f319
Israel <israel-mt.pstr@hotmail.com>
2019-12-05 10:21:58 +0100

    Cambios clase metodos de char a double

--- src/Ejercicios/Metodos.java
+++ src/Ejercicios/Metodos.java
@@ -5,6 +5,7 @@ public class Metodos {
 	public static void main (String[]args) {
 		System.out.println(devolucionVar());
 		System.out.println(numEnt(76));
+		System.out.println(numDec('L'));
 	}
 	public static String var1 () {
 	
@@ -21,6 +22,11 @@ public class Metodos {
 	
 	public static char numEnt (int var1) {
 		char var2 =  (char) var1;
+		return var2;
+		
+	}
+	public static double numDec (char var1) {
+		double var2 = (double) var1;
 		return var2;
 		
 	}
