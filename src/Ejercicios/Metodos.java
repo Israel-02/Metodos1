70991e31ca3a0a73422f2fcf38bc18b2565ea39e
Israel <israel-mt.pstr@hotmail.com>
2019-12-05 09:45:47 +0100

    Cambio clase metodos

--- src/Ejercicios/Metodos.java
+++ src/Ejercicios/Metodos.java
@@ -2,4 +2,19 @@ package Ejercicios;
 
 public class Metodos {
 
+	public static void main (String[]args) {
+		System.out.println(devolucionVar());
+	}
+	public static String var1 () {
+	
+		String var1 ="hola";
+		String var2 = ", que tal?";
+		String varfinal = var1+var2;
+		return varfinal;
+	}
+	
+	public static String devolucionVar() {
+		var1();
+	return var1();
+	}
 }
